a3020c9632343598b058dd2248b355ea442faebb
Viet Phan <phantuanviet0706@gmail.com>
2022-11-02 02:48:15 +0700

    Finish create & binding data Member

--- BusinessObject/Member.cs
+++ BusinessObject/Member.cs
@@ -12,22 +12,52 @@ namespace BusinessObject
         }
 
         public int MemberId { get; set; }
-        [Required(AllowEmptyStrings = false ,ErrorMessage = "EMAIL_EMPTY")]
+        [Required]
         [StringLength(100, ErrorMessage = "EMAIL_IN_INTERVAL_0_100")]
-        public string Email { get; set; } = null!;
-        [Required(AllowEmptyStrings = false, ErrorMessage = "COMPANY_NAME_EMPTY")]
+        public string Email { get; set; }
+        [Required]
         [StringLength(40, ErrorMessage = "COMPANY_NAME_IN_INTERVAL_0_40")]
-        public string CompanyName { get; set; } = null!;
-        [Required(AllowEmptyStrings = false, ErrorMessage = "CITY_EMPTY")]
+        public string CompanyName { get; set; }
+        [Required]
         [StringLength(15, ErrorMessage = "CITY_IN_INTERVAL_0_15")]
-        public string City { get; set; } = null!;
-        [Required(AllowEmptyStrings = false, ErrorMessage = "COUNTRY_EMPTY")]
+        public string City { get; set; }
+        [Required]
         [StringLength(15, ErrorMessage = "COUNTRY_IN_INTERVAL_0_15")]
-        public string Country { get; set; } = null!;
-        [Required(AllowEmptyStrings = false, ErrorMessage = "PASSWORD_EMPTY")]
+        public string Country { get; set; }
+        [Required]
         [StringLength(30, ErrorMessage = "PASSWORD_IN_INTERVAL_0_30")]
-        public string Password { get; set; } = null!;
+        public string Password { get; set; }
 
         public virtual ICollection<Order> Orders { get; set; }
+
+        public Member(int memberId, string email, string companyName, string city, string country, string password, ICollection<Order> orders)
+        {
+            MemberId = memberId;
+            Email = email;
+            CompanyName = companyName;
+            City = city;
+            Country = country;
+            Password = password;
+            Orders = orders;
+        }
+
+        public Member(string email, string companyName, string city, string country, string password)
+        {
+            Email = email;
+            CompanyName = companyName;
+            City = city;
+            Country = country;
+            Password = password;
+        }
+
+        public Member(string email, string companyName, string city, string country, string password, ICollection<Order> orders)
+        {
+            Email = email;
+            CompanyName = companyName;
+            City = city;
+            Country = country;
+            Password = password;
+            Orders = orders;
+        }
     }
 }
--- SalesWPFApp/ViewModels/MemberViewModel.cs
+++ SalesWPFApp/ViewModels/MemberViewModel.cs
@@ -3,7 +3,9 @@ using DataAccess.Repository;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,68 +16,78 @@ namespace SalesWPFApp.ViewModels
 {
     public class MemberViewModel : BaseViewModel
     {
+        private string _email;
+        public string Email { get => _email; set { _email = value; OnPropertyChanged(); } }
+
+        private string _companyName;
+        public string CompanyName { get => _companyName; set { _companyName = value; OnPropertyChanged(); } }
+
+        private string _city;
+        public string City { get => _city; set { _city = value; OnPropertyChanged(); } }
+
+        private string _country;
+        public string Country { get => _country; set { _country = value; OnPropertyChanged(); } }
+
+        private string _password;
+        public string Password { get => _password; set { _password = value; OnPropertyChanged(); } }
+
+
         MemberRepository memberRepository;
         public ICommand AddMemberCommand { get; set; }
         public ICommand CloseCommand { get; set; }
         public ObservableCollection<Member> memberList { get; set; }
+        public ICommand PasswordChangedCommand { get; set; }
+
+        private Member _selectedItem;
+        public Member SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value; 
+                OnPropertyChanged();
+                if(SelectedItem != null)
+                {
+                    Email = SelectedItem.Email;
+                    CompanyName = SelectedItem.CompanyName;
+                    City = SelectedItem.City;
+                    Country = SelectedItem.Country;
+                }
+            }
+        }
+
         public MemberViewModel()
         {
             this.memberRepository = new MemberRepository();
             memberList = new ObservableCollection<Member>();
 
             loadMemberList();
-            AddMemberCommand = new RelayCommand<UIElementCollection>((p) => { return true; }, (p) =>
+            PasswordChangedCommand = new RelayCommand<PasswordBox>((p) => { return true; }, (p) => { Password = p.Password; });
+
+            AddMemberCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
             {
-                string email = null;
-                string companyName = null;
-                string city = null;
-                string country = null;
-                string password = null;
-                Boolean isAllValid = true;
+                if (p == null)
+                {
+                    return;
+                }
+
+                if (Password == null)
+                {
+                    MessageBox.Show("Mật khẩu không được để trống!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                string passEncode = MD5Hash(Base64Encode(Password));
+                Member member = new Member(Email, CompanyName, City, Country, Password);
                 try
                 {
-                    foreach (var i in p)
-                    {
-                        TextBox tb = i as TextBox;
-                        if (tb != null)
-                        {
-
-                            switch (tb.Name)
-                            {
-                                case "email":
-                                    email = tb.Text;
-                                    break;
-                                case "companyName":
-                                    companyName = tb.Text;
-                                    break;
-                                case "city":
-                                    city = tb.Text;
-                                    break;
-                                case "country":
-                                    country = tb.Text;
-                                    break;
-                                case "password":
-                                    password = tb.Text;
-                                    break;
-                            }
-
-
-                        }
-
-                    }
+                    memberRepository.InsertMember(member);
+                    MessageBox.Show("Thêm thành viên thành công!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.None);
                 }
                 catch (Exception ex)
                 {
-                    isAllValid = false;
+                    MessageBox.Show(ex.Message, "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
-                if (isAllValid == true)
-                {
-                    Member member = new Member(email, companyName, city, country, password);
-                    memberRepository.InsertMember(new Member(email, companyName, city, country, password));
-                    loadMemberList();
-                }
-                else
-                    return;
+                loadMemberList();
             });
 
             CloseCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
@@ -96,6 +108,24 @@ namespace SalesWPFApp.ViewModels
             {
                 memberList.Add(member);
             }
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider mD5CryptoServiceProvider = new MD5CryptoServiceProvider();
+            byte[] bytes = mD5CryptoServiceProvider.ComputeHash(new UTF8Encoding().GetBytes(input));
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
         }
     }
 }
